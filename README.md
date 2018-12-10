5db28619f684d222035b198defb73564a8df995c
Joseph Lee <joseph.lee@uptake.com>
2018-12-09 20:54:11 -0600

    Added yml and put all data objects into the data folder

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
 # ehr-big-data
+
 Image processing tool for Electronic Health Records (EHR) using Spark, HDFS, Tesseract, and ElasticSearch
+
+
+
+# Setup
+1. Clone this repo
+2. Import and activate the conda environment. All following steps must be done in this environment.
+
+```bash
+conda env create -f environment.yml
+source activate ehr-big-data
+```
--- ocr_tesseract.py
+++ ocr_tesseract.py
@@ -14,15 +14,15 @@ collection = db.ocrdata
 
 pdf_file = 'Bariatrics_1_3.pdf'
 jpg_file = pdf_file[:-4] + '.jpg'
-with WImage(filename='pdf/' + pdf_file, resolution=300) as converted_image:
+with WImage(filename='data/pdf/' + pdf_file, resolution=300) as converted_image:
     converted_image.compression_quality = 99
-    converted_image.save(filename='image/' + jpg_file)
+    converted_image.save(filename='data/image/' + jpg_file)
     if len(converted_image.sequence) > 1:
-        output_images = ['image/' + jpg_file[:-4] + '-{0.index}.jpg'.format(x) for x in converted_image.sequence]
+        output_images = ['data/image/' + jpg_file[:-4] + '-{0.index}.jpg'.format(x) for x in converted_image.sequence]
     else:
-        output_images = ['image/' + jpg_file]
+        output_images = ['data/image/' + jpg_file]
 
-for output_image in output_images: 
+for output_image in output_images:
     with Image.open(output_image) as image_reader:
         ocr_text = pytesseract.image_to_string(image_reader)
 
@@ -45,7 +45,7 @@ for output_image in output_images:
             if header_flag == 1:
                 header_flag = 0
                 continue
-            
+
             # Skip rows without text
             if row[10] == "-1":
                 continue
@@ -68,12 +68,12 @@ for output_image in output_images:
                 ocr_json[ocr_key].append(ocr_value)
             else:
                 ocr_json[ocr_key] = [ocr_value]
-            
+
 
         image_buffer = BytesIO()
         image_reader.save(image_buffer, format="JPEG")
         image_str = base64.b64encode(image_buffer.getvalue())
-    
+
         mongo_row = {
             'image_name': output_image
             ,'image_file': image_str
@@ -85,5 +85,3 @@ for output_image in output_images:
         mongo_row_id = collection.insert_one(mongo_row).inserted_id
 
         # print(row_id)
-
-    
